6ba1584ee98d69ea490167f0f930a2777a850aee
linus-rc <linus.rudbeck@distansakademin.se>
2023-03-07 07:47:43 +0100

    Build project

---
+++ CompanyWebsite0/CompanyWebsite0/Controllers/EmployeesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CompanyWebsite0.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
